43aa8fe97cc457b1ce2fbdb4273eecb5057df220
zora1997 <97497973+zora1997@users.noreply.github.com>
2022-04-28 13:11:42 +0800

    Create deep_copy.go

---
+++ awesomego/copy/deep_copy.go
@@ -0,0 +1,50 @@
+package playground
+
+import "fmt"
+
+type testStruct struct {
+	uid  uint64
+	name string
+}
+
+func Data() (orign []*testStruct) {
+	dataOne := &testStruct{
+		uid:  111,
+		name: "dataOne",
+	}
+
+	dataTwo := &testStruct{
+		uid:  222,
+		name: "dataTwo",
+	}
+
+	dataThree := &testStruct{
+		uid:  333,
+		name: "dataThree",
+	}
+
+	orign = append(orign, dataOne, dataTwo, dataThree)
+
+	return orign
+}
+
+func DeepCopy() (result []*testStruct) {
+	origin := Data()
+	for i := range origin {
+		result = append(result, &testStruct{
+			uid:  origin[i].uid,
+			name: origin[i].name,
+		})
+	}
+	fmt.Printf("result: %v", result)
+
+	return result
+}
+
+func ShallowCopy() (result []*testStruct) {
+	origin := Data()
+	result = origin
+	fmt.Printf("result: %v", result)
+
+	return result
+}
